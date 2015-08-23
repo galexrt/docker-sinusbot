07bf6e7db6cc913accc4b090ecf0f196dad2654d
Alexander Trost <galexrt@googlemail.com>
2015-08-23 18:29:24 +0200

    Could may fix the TSClient quit error now

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Starting TeamSpeak 3 SinusBot ..."
-LC_ALL="en_US.UTF-8" xinit $SINUS_DIR/ts3bot -- /usr/bin/Xvfb :1 -screen 0 800x600x16 -ac
+su -c "LC_ALL=\"en_US.UTF-8\" xinit $SINUS_DIR/ts3bot -- /usr/bin/Xvfb :1 -screen 0 800x600x16 -ac" "$SINUS_USER"
