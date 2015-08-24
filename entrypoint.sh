643f1b521a7253b53c8a6d85ea0e61df1e441b48
Alexander Trost <galexrt@googlemail.com>
2015-08-24 11:55:16 +0200

    Quick fix for wrong sudo args

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Correcting mount point permissions ..."
 chown "$SINUS_USER":"$SINUS_GROUP" -R "$SINUS_DATA"
 
 echo "Checking for TeamSpeak SinusBot Updates ..."
-sudo -u "$SINUS_USER" -c "$SINUS_DIR/ts3bot -update"
+sudo -u "$SINUS_USER" "$SINUS_DIR/ts3bot -update"
 
 echo "Starting TeamSpeak SinusBot ..."
-sudo -u "$SINUS_USER" -c "xinit $SINUS_DIR/ts3bot -- /usr/bin/Xvfb :1 -screen 0 800x600x16 -ac"
+sudo -u "$SINUS_USER" "xinit $SINUS_DIR/ts3bot -- /usr/bin/Xvfb :1 -screen 0 800x600x16 -ac"
