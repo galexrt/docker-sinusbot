0576b245d0c0da86d8910d75d8d256d0dad31c91
Alexander Trost <galexrt@googlemail.com>
2016-08-24 14:11:01 +0200

    Updated to latest beta

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,8 @@ if [ "$DEBUG" == "True" ] || [ "$DEBUG" == "true" ]; then
     sed -i 's/LogLevel.*/LogLevel = 10/g' "$SINUS_DIR/config.ini"
 fi
 
-echo "Removing /tmp/.X1-lock, if existing"
-rm -f /tmp/.X1-lock
-
 echo "Correcting mount point permissions ..."
 chown "$SINUS_USER":"$SINUS_GROUP" -R "$SINUS_DATA"
 
-echo "Checking for TeamSpeak SinusBot Updates ..."
-sudo -u "$SINUS_USER" -g "$SINUS_GROUP" "$SINUS_DIR/ts3bot" -update
-
 echo "Starting TeamSpeak SinusBot ..."
-exec sudo -u "$SINUS_USER" -g "$SINUS_GROUP" xinit "$SINUS_DIR/ts3bot" -- /usr/bin/Xvfb :1 -screen 0 800x600x16 -ac
+exec sudo -u "$SINUS_USER" -g "$SINUS_GROUP" "$SINUS_DIR/sinusbot"
