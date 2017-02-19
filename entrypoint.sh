b4000c8d58ae480c189cedfa938f115bf5c2a949
R0GGER <rogger@spotx.nu>
2017-02-19 01:48:08 +0100

    update youtube-dl before start sinusbot

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,6 +35,8 @@ if [ -d "/data" ]; then
 else
     echo "=> You are good to go! You are already using the new data directory, located at \"$SINUS_DATA\"."
 fi
+echo "=> Update Youtube-dl"
+$YTDL_BIN -U
 
 echo "=> Starting SinusBot (https://sinusbot.com) by Michael Friese ..."
 exec sudo -u sinusbot -g sinusbot "$SINUS_DIR/sinusbot"
