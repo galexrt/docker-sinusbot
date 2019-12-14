5f4093e19ff877dcd3db18d001abdb01ccb4c1b9
Alexander Trost <galexrt@googlemail.com>
2019-12-14 14:37:37 +0100

    entrypoint: copy config if doesn't exist in volume
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,13 +15,13 @@ else
     echo "=> Scripts directory is marked, scripts were already copied. Nothing to do."
 fi
 
-if [ -d "$SINUS_CONFIG" ] && [ -f "$SINUS_CONFIG/config.ini" ]; then
-    echo "-> Found config in $SINUS_CONFIG with config.ini directory, linking ..."
+if [ -d "$SINUS_CONFIG" ]; then
+    if [ ! -f "$SINUS_CONFIG/config.ini" ]; then
+        cp -a "$SINUS_DIR/config.ini" "$SINUS_CONFIG/config.ini"
+    fi
     rm -f "$SINUS_DIR/config.ini"
     ln -sf "$SINUS_CONFIG/config.ini" "$SINUS_DIR/config.ini"
     echo "=> Linked $SINUS_CONFIG/config.ini to $SINUS_DIR/config.ini."
-else
-    echo "-> No $SINUS_CONFIG/config.ini found, not linking."
 fi
 
 echo "=> Starting SinusBot (https://sinusbot.com) by Michael Friese ..."
